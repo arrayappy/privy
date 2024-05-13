91c15dd7bcaafe9971bca78c754eb138ca480257
arrayappy <arrayappy@gmail.com>
2024-05-13 22:05:20 +0530

    added pda to user account creation

--- programs/privy/src/lib.rs
+++ programs/privy/src/lib.rs
@@ -6,10 +6,60 @@ declare_id!("DRpfXBx35Z5QDTnQzV756avabprLWqfbbo3jxAa66ThD");
 pub mod privy {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_user(ctx: Context<CreateUser>, username: String, passkey: String) -> Result<()> {
+        let privy_user = &mut ctx.accounts.privy_user;
+        privy_user.username = username;
+        privy_user.passkey = passkey;
+        privy_user.disabled = false;
+        privy_user.bump = ctx.bumps.privy_user;
+
+        Ok(())
+    }
+
+    pub fn change_username(ctx: Context<ChangeUserName>, new_username: String) -> Result<()> {
+        ctx.accounts.privy_user.username = new_username;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateUser<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        // space = 8 + PrivyUser::INIT_SPACE,
+        space = 8 + 1000,
+        seeds = [b"privy-user", user.key().as_ref()],
+        bump
+    )]
+    pub privy_user: Account<'info, PrivyUser>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ChangeUserName<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"privy-user", user.key().as_ref()],
+        bump = privy_user.bump
+    )]
+    pub privy_user: Account<'info, PrivyUser>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct PrivyUser {
+    #[max_len(24)]
+    pub username: String,
+    #[max_len(24)]
+    pub passkey: String,
+    pub disabled: bool,
+    pub bump: u8
+}
--- tests/privy.ts
+++ tests/privy.ts
@@ -1,16 +1,62 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { Privy } from "../target/types/privy";
+import { expect } from 'chai';
+import { off } from "process";
 
 describe("privy", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
   const program = anchor.workspace.Privy as Program<Privy>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // const privy = anchor.web3.Keypair.generate();
+
+  const user1 = {
+    username: "arrayappy",
+    passkey: "secret"
+  }
+
+  const [privyUserPDA, _] = PublicKey.findProgramAddressSync(
+    [
+      anchor.utils.bytes.utf8.encode('privy-user'),
+      provider.wallet.publicKey.toBuffer()
+    ],
+    program.programId
+  );
+
+  it("Create user 1", async () => {
+
+    await program.methods
+      .createUser(user1.username, user1.passkey)
+      .accounts({
+        user: provider.wallet.publicKey,
+        privyUser: privyUserPDA,
+      })
+      .rpc()
+
+      const accountData = await program.account.privyUser.fetch(privyUserPDA)
+      console.log(accountData);
+      expect((accountData.username)).to.equal(user1.username);
   });
+  
+
+  it("Change username", async() => {
+    const changedUsername = "arrayappy2";
+    await program.methods
+      .changeUsername(changedUsername)
+      .accounts({
+        user: provider.wallet.publicKey,
+        privyUser: privyUserPDA
+      })
+      .rpc()
+
+      const accountData = await program.account.privyUser.fetch(privyUserPDA)
+      console.log(accountData);
+      expect((accountData.username)).to.equal(changedUsername);
+  });
+
+  
 });
