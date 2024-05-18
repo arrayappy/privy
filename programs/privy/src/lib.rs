cc1dfc67a397ae39b7cb76d1a5976c584ff4842b
arrayappy <arrayappy@gmail.com>
2024-05-18 15:47:12 +0530

    feat: transferring sol to pda via cpi calls

--- programs/privy/src/lib.rs
+++ programs/privy/src/lib.rs
@@ -1,65 +1,169 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{self, Transfer};
+use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::solana_program::sysvar::rent::Rent;
 
-declare_id!("DRpfXBx35Z5QDTnQzV756avabprLWqfbbo3jxAa66ThD");
+declare_id!("GW79qwoxeZLoyN1wBwDmJ19CjmqsL5rD44jBDCRnvyie");
+
+const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
 
 #[program]
 pub mod privy {
     use super::*;
 
-    pub fn create_user(ctx: Context<CreateUser>, username: String, passkey: String) -> Result<()> {
-        let privy_user = &mut ctx.accounts.privy_user;
-        privy_user.username = username;
-        privy_user.passkey = passkey;
-        privy_user.disabled = false;
-        privy_user.bump = ctx.bumps.privy_user;
-
+    pub fn initialize_privy_config(ctx: Context<InitializePrivyConfig>, tokens_per_sol: u32) -> Result<()> {
+        let privy_config = &mut ctx.accounts.privy_config;
+        privy_config.tokens_per_sol = tokens_per_sol;
+        msg!("Initialized privy config with {} tokens per SOL", tokens_per_sol);
         Ok(())
     }
 
-    pub fn change_username(ctx: Context<ChangeUserName>, new_username: String) -> Result<()> {
-        ctx.accounts.privy_user.username = new_username;
+    pub fn update_privy_config(ctx: Context<UpdatePrivyConfig>, tokens_per_sol: u32) -> Result<()> {
+        let privy_config = &mut ctx.accounts.privy_config;
+        privy_config.tokens_per_sol = tokens_per_sol;
+        msg!("Updated privy config to {} tokens per SOL", tokens_per_sol);
+        Ok(())
+    }
+
+    pub fn create_user(ctx: Context<CreateUser>, username: String, passkey: String, deposit_lamports: u64) -> Result<()> {
+        let privy_user = &mut ctx.accounts.privy_user;
+        let privy_config = &ctx.accounts.privy_config;
+        let deposit_sol = deposit_lamports as f64 / LAMPORTS_PER_SOL as f64;
+        let vector_size = (deposit_sol * privy_config.tokens_per_sol as f64).floor() as usize;
+    
+        // msg!("deposit_lamports: {}", deposit_lamports);
+        // msg!("deposit_sol: {}", deposit_sol);
+        // msg!("tokens_per_sol: {}", privy_config.tokens_per_sol);
+        // msg!("vector_size: {}", vector_size);
+    
+        privy_user.username = username.clone();
+        privy_user.passkey = passkey;
+        privy_user.disabled = false;
+        privy_user.bump = ctx.bumps.privy_user;
+        privy_user.tokens = Vec::with_capacity(vector_size);
+        privy_user.token_limit = vector_size as u16;
+        // privy_user.token_limit = privy_user.tokens.capacity() as u16;
+    
+        msg!("Creating user {} with deposit of {} lamports ({} SOL), resulting in token capacity of {}", username, deposit_lamports, deposit_sol, vector_size);
+    
+        // Transfer lamports from user to program
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.user.to_account_info(),
+            to: ctx.accounts.privy_user.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.system_program.to_account_info();
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        system_program::transfer(cpi_context, deposit_lamports)?;
+    
+        msg!("Transferred {} lamports from user to privy_user account", deposit_lamports);
+    
+        Ok(())
+    }
+
+    pub fn add_tokens(ctx: Context<AddTokens>, additional_lamports: u64) -> Result<()> {
+        let privy_user = &mut ctx.accounts.privy_user;
+        let privy_config = &ctx.accounts.privy_config;
+        let additional_sol = additional_lamports as f64 / LAMPORTS_PER_SOL as f64;
+        let additional_tokens = (additional_sol * privy_config.tokens_per_sol as f64).floor() as usize;
+
+        privy_user.tokens.reserve(additional_tokens);
+        privy_user.token_limit = privy_user.token_limit.checked_add(additional_tokens as u16).unwrap();
+
+        msg!("Adding {} lamports ({} SOL) to user account, resulting in additional token capacity of {}", additional_lamports, additional_sol, additional_tokens);
+
+        // Transfer lamports from user to program
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.user.to_account_info(),
+            to: ctx.accounts.privy_user.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.system_program.to_account_info();
+        let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+        system_program::transfer(cpi_context, additional_lamports)?;
+
+        msg!("Transferred {} lamports from user to privy_user account", additional_lamports);
 
         Ok(())
     }
 }
+
+#[derive(Accounts)]
+pub struct InitializePrivyConfig<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + PrivyConfig::LEN,
+        seeds = [b"privy-config", owner.key().as_ref()],
+        bump
+    )]
+    pub privy_config: Account<'info, PrivyConfig>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdatePrivyConfig<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(mut)]
+    pub privy_config: Account<'info, PrivyConfig>,
+    pub system_program: Program<'info, System>,
+}
+
 
 #[derive(Accounts)]
 pub struct CreateUser<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
-
     #[account(
         init,
         payer = user,
-        // space = 8 + PrivyUser::INIT_SPACE,
-        space = 8 + 1000,
+        space = 8 + PrivyUser::INIT_SPACE,
         seeds = [b"privy-user", user.key().as_ref()],
         bump
     )]
     pub privy_user: Account<'info, PrivyUser>,
+    pub privy_config: Account<'info, PrivyConfig>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct ChangeUserName<'info> {
+pub struct AddTokens<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
-
-    #[account(
-        mut,
-        seeds = [b"privy-user", user.key().as_ref()],
-        bump = privy_user.bump
-    )]
+    #[account(mut)]
     pub privy_user: Account<'info, PrivyUser>,
+    pub privy_config: Account<'info, PrivyConfig>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
-#[derive(InitSpace)]
+pub struct PrivyConfig {
+    pub tokens_per_sol: u32,
+}
+
+impl PrivyConfig {
+    const LEN: usize = 8 + 4;
+}
+
+#[account]
 pub struct PrivyUser {
-    #[max_len(24)]
     pub username: String,
-    #[max_len(24)]
     pub passkey: String,
     pub disabled: bool,
-    pub bump: u8
+    pub token_limit: u16,
+    pub tokens: Vec<String>,
+    pub bump: u8,
+}
+
+impl PrivyUser {
+    const INIT_SPACE: usize = 8 + 4 + 24 + 1 + 1 + (140 * 10); // Approximate initial space calculation
+}
+
+#[error_code]
+pub enum CustomError {
+    #[msg("Unauthorized.")]
+    Unauthorized,
+    #[msg("Bump value not found.")]
+    BumpNotFound,
 }
--- tests/privy.ts
+++ tests/privy.ts
@@ -1,25 +1,31 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 import { Privy } from "../target/types/privy";
 import { expect } from 'chai';
-import { off } from "process";
 
 describe("privy", () => {
+  // Initialize the provider and set it as the default provider
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
 
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-
+  // Define the program instance
   const program = anchor.workspace.Privy as Program<Privy>;
-
-  // const privy = anchor.web3.Keypair.generate();
-
-  const user1 = {
+  console.log(program.programId.toString())
+  // Define the PDA for the privy config account
+  const [privyConfigPDA, _] = PublicKey.findProgramAddressSync(
+    [anchor.utils.bytes.utf8.encode('privy-config'), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  );
+  console.log('privyConfigPDA', privyConfigPDA.toString())
+  // Define user data for the tests
+  const userData = {
     username: "arrayappy",
     passkey: "secret"
-  }
+  };
 
-  const [privyUserPDA, _] = PublicKey.findProgramAddressSync(
+  // Define the PDA for the privy user account
+  const [privyUserPDA, bump] = PublicKey.findProgramAddressSync(
     [
       anchor.utils.bytes.utf8.encode('privy-user'),
       provider.wallet.publicKey.toBuffer()
@@ -27,36 +33,79 @@ describe("privy", () => {
     program.programId
   );
 
-  it("Create user 1", async () => {
+
+  it("Initialize Privy Config", async () => {
+    const tokensPerSol = 10;
+    await program.methods
+      .initializePrivyConfig(tokensPerSol)
+      .accounts({
+        privyConfig: privyConfigPDA,
+        owner: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+
+    // Fetch and verify the privy config account data
+    const configData = await program.account.privyConfig.fetch(privyConfigPDA);
+    console.log(configData);
+    expect(configData.tokensPerSol).to.equal(tokensPerSol);
+  });
+
+  // it("Update Privy Config", async () => {
+  //   const newTokensPerSol = 20;
+
+  //   await program.methods
+  //     .updatePrivyConfig(newTokensPerSol)
+  //     .accounts({
+  //       privyConfig: privyConfigPDA,
+  //       owner: provider.wallet.publicKey,
+  //     })
+  //     .rpc();
+
+  //   // Fetch and verify the updated privy config account data
+  //   const updatedConfigData = await program.account.privyConfig.fetch(privyConfigPDA);
+  //   console.log(updatedConfigData);
+  //   expect(updatedConfigData.tokensPerSol).to.equal(newTokensPerSol);
+  // });
+
+  it("Create User", async () => {
+    const depositLamports = 1 * anchor.web3.LAMPORTS_PER_SOL; // 1 SOL
+
+    const tx = await program.methods
+        .createUser(userData.username, userData.passkey, new anchor.BN(depositLamports))
+        .accounts({
+            user: provider.wallet.publicKey,
+            privyUser: privyUserPDA,
+            privyConfig: privyConfigPDA,
+            systemProgram: SystemProgram.programId,
+        })
+        .rpc();
+
+    console.log('tx', tx)
+    const accountData = await program.account.privyUser.fetch(privyUserPDA);
+    console.log("accountData", accountData);
+    expect(accountData.username).to.equal(userData.username);
+    expect(accountData.tokenLimit).to.equal(Math.floor(depositLamports / anchor.web3.LAMPORTS_PER_SOL * 10)); // Ensure tokens length is as expected
+});
+
+
+  it("Add tokens", async () => {
+    const additionalLamports = 1 * anchor.web3.LAMPORTS_PER_SOL; // 1 SOL
 
     await program.methods
-      .createUser(user1.username, user1.passkey)
+      .addTokens(new anchor.BN(additionalLamports))
       .accounts({
         user: provider.wallet.publicKey,
         privyUser: privyUserPDA,
+        privyConfig: privyConfigPDA,
       })
-      .rpc()
+      .rpc();
 
-      const accountData = await program.account.privyUser.fetch(privyUserPDA)
-      console.log(accountData);
-      expect((accountData.username)).to.equal(user1.username);
-  });
-  
-
-  it("Change username", async() => {
-    const changedUsername = "arrayappy2";
-    await program.methods
-      .changeUsername(changedUsername)
-      .accounts({
-        user: provider.wallet.publicKey,
-        privyUser: privyUserPDA
-      })
-      .rpc()
-
-      const accountData = await program.account.privyUser.fetch(privyUserPDA)
-      console.log(accountData);
-      expect((accountData.username)).to.equal(changedUsername);
+    // Fetch and verify the privy user account data after adding tokens
+    const accountData = await program.account.privyUser.fetch(privyUserPDA);
+    console.log(accountData);
+    const expectedTokensLength = Math.floor((2 * anchor.web3.LAMPORTS_PER_SOL) / anchor.web3.LAMPORTS_PER_SOL * 10); // Initial deposit + additional SOL
+    expect(accountData.tokenLimit).to.equal(expectedTokensLength); // Ensure the vector length is as expected
   });
 
-  
 });
