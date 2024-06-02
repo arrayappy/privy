a62516856992c55d71b0e66779f525423f9c2ea1
arrayappy <arrayappy@gmail.com>
2024-06-02 18:33:17 +0530

    feat: added withdraw balance and update user instructions

--- programs/privy/src/lib.rs
+++ programs/privy/src/lib.rs
@@ -26,6 +26,14 @@ pub mod privy {
         Ok(())
     }
 
+    pub fn withdraw_balance(ctx: Context<WithdrawBalance>, amount: u64) -> Result<()> {
+        ctx.accounts.privy_config.sub_lamports(amount)?;
+        ctx.accounts.owner.add_lamports(amount)?;
+
+        msg!("Withdrawn {} lamports to owner", amount);
+        Ok(())
+    }
+
     pub fn create_user(ctx: Context<CreateUser>, username: String, passkey: String, deposit_lamports: u64) -> Result<()> {
         let user = &mut ctx.accounts.user;
         let privy_user = &mut ctx.accounts.privy_user;
@@ -51,10 +59,52 @@ pub mod privy {
         privy_user.passkey = passkey;
         privy_user.disabled = false;
         privy_user.bump = ctx.bumps.privy_user;
-        privy_user.tokens = Vec::with_capacity(vector_size);
+        privy_user.messages = Vec::with_capacity(vector_size);
         privy_user.token_limit = vector_size as u16;
-        privy_user.compute_space = computed_space as u32;
-        privy_user.total_sol = privy_user.get_lamports() as f64;
+    
+        let cpi_accounts = Transfer {
+            from: user.to_account_info(),
+            to: privy_user.to_account_info(),
+        };
+        let cpi_context = CpiContext::new(system_program.to_account_info(), cpi_accounts);
+        system_program::transfer(cpi_context, privy_user_lamports)?;
+
+        let cpi_accounts_config = Transfer {
+            from: user.to_account_info(),
+            to: privy_config.to_account_info(),
+        };
+        let cpi_context_config = CpiContext::new(system_program.to_account_info(), cpi_accounts_config);
+        system_program::transfer(cpi_context_config, privy_config_lamports)?;
+    
+        Ok(())
+    }
+
+    pub fn update_user(ctx: Context<UpdateUser>, username: String, passkey: String, disabled: bool) -> Result<()> {
+        let privy_user = &mut ctx.accounts.privy_user;
+        privy_user.username = username;
+        privy_user.passkey = passkey;
+        privy_user.disabled = disabled;
+        Ok(())
+    }
+    
+    pub fn add_tokens(ctx: Context<AddTokens>, additional_lamports: u64,) -> Result<()> {
+        let user = &mut ctx.accounts.user;
+        let privy_user = &mut ctx.accounts.privy_user;
+        let privy_config = &mut ctx.accounts.privy_config;
+        let system_program = &ctx.accounts.system_program;
+        
+        let additional_sol = additional_lamports as f64 / LAMPORTS_PER_SOL as f64;
+        let additional_tokens = (additional_sol * privy_config.tokens_per_sol as f64).floor() as usize;
+    
+        let new_token_limit = privy_user.token_limit.checked_add(additional_tokens as u16).ok_or(CustomError::TokenLimitExceeded)?;
+        let addtional_computed_space = additional_tokens * (4 + 140);
+
+        let rent = &ctx.accounts.rent;
+        let privy_user_lamports = rent.minimum_balance(addtional_computed_space);
+        let privy_config_lamports = additional_lamports.saturating_sub(privy_user_lamports);
+    
+        privy_user.token_limit = new_token_limit;
+        privy_user.messages.reserve(new_token_limit as usize);
     
         let cpi_accounts = Transfer {
             from: user.to_account_info(),
@@ -74,59 +124,14 @@ pub mod privy {
     }
     
     pub fn allocate_space(ctx: Context<AllocateSpace>, computed_space: u32) -> Result<()> {
-        let privy_user = &mut ctx.accounts.privy_user;
-        privy_user.compute_space = privy_user.compute_space.checked_add(computed_space).unwrap();
-        msg!("{}", privy_user.compute_space);
         ctx.accounts.privy_user.to_account_info().realloc(computed_space as usize, false)?;
         Ok(())
     }
 
-    pub fn add_tokens(ctx: Context<AddTokens>, additional_lamports: u64,) -> Result<()> {
-        let user = &mut ctx.accounts.user;
-        let privy_user = &mut ctx.accounts.privy_user;
-        let privy_config = &mut ctx.accounts.privy_config;
-        let system_program = &ctx.accounts.system_program;
-        
-        let additional_sol = additional_lamports as f64 / LAMPORTS_PER_SOL as f64;
-        let additional_tokens = (additional_sol * privy_config.tokens_per_sol as f64).floor() as usize;
-    
-        let new_token_limit = privy_user.token_limit.checked_add(additional_tokens as u16).ok_or(CustomError::TokenLimitExceeded)?;
-        let addtional_computed_space = additional_tokens * (4 + 140);
-        let new_computed_space = privy_user.compute_space + addtional_computed_space as u32;
-
-        let rent = &ctx.accounts.rent;
-        let privy_user_lamports = rent.minimum_balance(addtional_computed_space);
-        let privy_config_lamports = additional_lamports.saturating_sub(privy_user_lamports);
-    
-        privy_user.token_limit = new_token_limit;
-        privy_user.compute_space = new_computed_space;
-        privy_user.total_sol = privy_user.get_lamports() as f64;
-        privy_user.tokens.reserve(new_token_limit as usize); // finalise
-    
-        let account_info = &mut privy_user.to_account_info();
-        account_info.realloc(new_computed_space as usize, true)?;
-
-        let cpi_accounts = Transfer {
-            from: user.to_account_info(),
-            to: privy_user.to_account_info(),
-        };
-        let cpi_context = CpiContext::new(system_program.to_account_info(), cpi_accounts);
-        system_program::transfer(cpi_context, privy_user_lamports)?;
-
-        let cpi_accounts_config = Transfer {
-            from: user.to_account_info(),
-            to: privy_config.to_account_info(),
-        };
-        let cpi_context_config = CpiContext::new(system_program.to_account_info(), cpi_accounts_config);
-        system_program::transfer(cpi_context_config, privy_config_lamports)?;
-    
-        Ok(())
-    }
-    
-
     pub fn insert_message(ctx: Context<InsertMessage>, message: String) -> Result<()> {
         let privy_user = &mut ctx.accounts.privy_user;
-        privy_user.tokens.push(message);
+        require!(!privy_user.disabled, CustomError::MessagesDisabled);
+        privy_user.messages.push(message);
         Ok(())
     }
 }
@@ -157,6 +162,17 @@ pub struct UpdatePrivyConfig<'info> {
 
 
 #[derive(Accounts)]
+pub struct WithdrawBalance<'info> {
+    #[account(mut, constraint = privy_config.owner == *owner.key @ CustomError::Unauthorized )]
+    pub payer: Signer<'info>,
+    #[account(mut)]
+    pub privy_config: Account<'info, PrivyConfig>,
+    /// CHECK: ignore
+    pub owner: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
 pub struct CreateUser<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
@@ -185,6 +201,17 @@ pub struct AddTokens<'info> {
     pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
 }
+
+
+#[derive(Accounts)]
+pub struct UpdateUser<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub privy_user: Account<'info, PrivyUser>,
+    pub system_program: Program<'info, System>,
+}
+
 
 #[derive(Accounts)]
 pub struct AllocateSpace<'info> {
@@ -226,9 +253,7 @@ pub struct PrivyUser {
     pub passkey: String,
     pub disabled: bool,
     pub token_limit: u16,
-    pub tokens: Vec<String>,
-    pub total_sol: f64,
-    pub compute_space: u32, 
+    pub messages: Vec<String>,
     pub bump: u8,
 }
 
@@ -247,4 +272,6 @@ pub enum CustomError {
     Unauthorized,
     #[msg("Token limit exceeded")]
     TokenLimitExceeded,
+    #[msg("Disabled receiving messages.")]
+    MessagesDisabled,
 }
