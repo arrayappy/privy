8bec1e6d454fd8c3f6d05d1faa61b29a3003bc18
arrayappy <arrayappy@gmail.com>
2024-11-11 22:43:20 +0530

    server: fix host

--- server/src/main.rs
+++ server/src/main.rs
@@ -288,7 +288,7 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("NO_DIESEL_PREPARED_STATEMENTS", "true");
 
     let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
-    let addr = format!("127.0.0.1:{}", port);
+    let addr = format!("0.0.0.0:{}", port);
 
     println!("Running on port {}...", port);
 
