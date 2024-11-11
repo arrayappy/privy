3a223f1eb4907eb1494d062a7374e87f53fe2467
arrayappy <arrayappy@gmail.com>
2024-11-11 22:13:20 +0530

    server: cors update

--- server/src/main.rs
+++ server/src/main.rs
@@ -272,7 +272,10 @@ async fn main() -> std::io::Result<()> {
     println!("Running on port {}...", port);
 
     HttpServer::new(|| {
-        let cors = Cors::permissive();
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header();
 
         App::new()
             .wrap(cors)
