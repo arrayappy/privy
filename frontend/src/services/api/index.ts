81e509db257884fb10436ae64e4fa90fe159d94b
arrayappy <arrayappy@gmail.com>
2024-12-03 21:29:20 +0530

    frontend: added backend apis

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,16 +1,42 @@
 import getApiUrl from "src/utils/api/getApiUrl";
 
-async function createUser(user_addr: string, user_name: string, password: string) {
-  // const { getPasswordSalt } = await import("@privy/sdk/utils/helpers");
-  // const { generateKeypair } = await import("@privy/sdk/utils/asymmetric");
-  
-  // const password_salt = getPasswordSalt(password);
-  // const { publicKeyPem } = generateKeypair(password_salt);
+async function createUser(user_addr: string, user_name: string, password_salt: string, password_pubkey: string) {
   const res = await fetch(`${getApiUrl()}/create_user`, {
     method: "POST",
-    // body: JSON.stringify({ user_addr, user_name, password_salt, password_pubkey: publicKeyPem }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user_addr, user_name, password_salt, password_pubkey }),
   });
   return res.json();
 }
 
-export { createUser };
+
+async function updateUser(user_addr: string, user_name: string, password_salt: string, password_pubkey: string) {
+  const res = await fetch(`${getApiUrl()}/update_user`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user_addr, user_name, password_salt, password_pubkey }),
+  });
+  return res.json();
+}
+
+async function getUser(user_name: string) {
+  const res = await fetch(`${getApiUrl()}/get_user?user_name=${user_name}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  return res.json();
+}
+
+async function getDbUser(user_addr: string) {
+  const res = await fetch(`${getApiUrl()}/get_db_user?user_addr=${user_addr}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  return res.json();
+}
+
+export { createUser, updateUser, getUser, getDbUser };
