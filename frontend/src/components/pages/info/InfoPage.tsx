4f4ccfd7ef731bca24cce8ecca61bb4a4c5482dc
arrayappy <arrayappy@gmail.com>
2024-12-08 19:39:17 +0530

    frontend: updated info page

--- frontend/src/components/pages/info/InfoPage.tsx
+++ frontend/src/components/pages/info/InfoPage.tsx
@@ -9,42 +9,45 @@ import ExternalLink from "src/components/links/ExternalLink";
 const ITEMS = [
   {
     description:
-      "Hammyflip is a simple coin flip game that runs on Solana devnet. If you win, you receive double the SOL you initally bet!",
-    title: "What is Hammyflip?",
-  },
-  {
-    description: "You have a 50/50 chance of winning the coin flip.",
-    title: "What are the odds of winning?",
+      "A trustless Solana app for receiving private messages securely and spam-free through a simple, sharable link.",
+    title: "What is Privy?",
   },
   {
     description: (
       <>
-        Hammyflip takes a 3% fee on your coin flip bet (e.g. betting 1 SOL costs
-        1.03 SOL).
+        In Privy, messages are referred to as "fruits" and categories are called "crates".
+      </>
+    ),
+    title: "What is the terminology used?",
+  },
+  {
+    description:
+      "The sender pays the transaction fees using a relayer backend service while ensuring client-side message encryption.",
+    title: "How does it work?",
+  },
+  {
+    description: (
+      <>
+        Privy currently operates on Devnet SOL. The exchange rate is 100 Privy tokens per Devnet SOL, with each Privy token allowing you to receive one message.
         <br />
         <br />
-        The first time you play, there is also a small one-time cost in order to
-        pay for{" "}
-        <ExternalLink href="https://docs.solana.com/terminology#rent">
-          rent
-        </ExternalLink>
-        . Subsequent plays will not include this cost.
+        Additionally, any account changes like updating settings or categories will require paying for an on-chain transaction.
       </>
     ),
     title: "What fees do you charge?",
   },
-  {
-    description: (
-      <>
-        Please DM our Twitter account (
-        <ExternalLink href="https://twitter.com/arrayappy">
-          @pencilflip
-        </ExternalLink>
-        ) with any questions or concerns.
-      </>
-    ),
-    title: "I need help, where do I go?",
-  },
+  // {
+  //   description: (
+  //     <>
+  //       Please DM our Twitter account (
+  //       <ExternalLink href="https://twitter.com/arrayappy">
+  //         @arrayappy
+  //       </ExternalLink>
+  //       ) with any questions or concerns.
+  //     </>
+  //   ),
+  //   title: "I need help, where do I go?",
+  // },
 ];
 
 function InfoItem({
